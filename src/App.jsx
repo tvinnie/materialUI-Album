b830f4370eaf07381bb457df2eaf02a940c72156
tvinnie <tvinnie77@gmail.com>
2022-10-15 17:54:39 +0300

    material ui album

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from "@mui/material";
+import { PhotoCamera } from '@mui/icons-material';
+import useStyles from "./styles";
+
+
+
+const cards =[1,2,3,4,5,6,7,8,9];
+
+
+const App = () => {
+    const {classes} = useStyles();
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Toolbar>
+                    <PhotoCamera  className={classes.icon}/>
+                    <Typography variant="h6">
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            {/* main body */}
+            <main>
+                <div className={classes.container}>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary">
+                            This is an Album
+                        </Typography>
+
+                        <div className={classes.buttons}>
+                            <Grid container spacing={2} justifyContent="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        See My Photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant="outlined" color="primary">
+                                        Secondary
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+                <Container className={classes.cardGrid} maxwidth='md'>
+                    <Grid container spacing={4}>
+                        {
+                            cards.map((card)=>(
+                                <Grid item key={card} xs={12} sm={6} md={4}>
+                                    <Card className={classes.card}>
+                                        <CardMedia 
+                                        className={classes.cardMedia}
+                                        image="https://source.unsplash.com/random"
+                                        title="Image Title"
+                                        />
+                                        <CardContent className={classes.cardContent}>
+                                            <Typography gutterBottom variant="h5">
+                                                Heading
+                                            </Typography>
+                                            <Typography>
+                                                This is media card you can use to descrbe your content.
+                                            </Typography>
+                                        </CardContent>
+                                        <CardActions >
+                                            <Button size='small' color='primary'>View</Button>
+                                            <Button size='small' color='primary'>Edit</Button>
+                                        </CardActions>
+                                    </Card>
+                                </Grid>
+                            ))
+                        }
+                        
+                    </Grid>
+
+                </Container>
+
+            </main>
+            <footer className={classes.footer}>
+                <Typography variant='h6' align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary">
+                    Do something to give purpose
+                </Typography>
+            </footer>
+
+        </>
+    )
+}
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,38 @@
+import {makeStyles} from "tss-react/mui";
+
+const useStyles = makeStyles()((theme) => {
+    
+    return {
+        container: {
+            backgroundColor:theme.palette.background.paper,
+            padding:theme.spacing(8,0,6)
+        },
+        icon: {
+            marginRight:' 20px',
+        },
+        buttons: {
+            marginTop: '40px',
+        },
+        cardGrid:{
+            padding: '20px 0'
+        },
+        card:{
+            height:'100%',
+            display: 'flex',
+            flexDirection: 'column'
+        },
+        cardMedia:{
+             paddingTop: ' 56.25%',
+        },
+        cardContent:{
+            flexGrow:1,
+        },
+        footer:{
+            backgroundColor:theme.palette.background.paper,
+            padding: '50px 0px'
+        }
+    }
+  
+})
+
+export default useStyles;
